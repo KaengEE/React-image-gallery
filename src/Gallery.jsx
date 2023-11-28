b2781ee55abd05383593f8cf3cfc1a242ecda693
KaengEE <yopuiqwer@naver.com>
2023-11-28 16:02:28 +0900

    갤러리 데이터 가져오기

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,7 +1,50 @@
 import React from "react";
+import { useGlobalContext } from "./context";
+import { useFetch } from "./useFetch";
+
+const url = `https://api.unsplash.com/search/photos?client_id=${
+  import.meta.env.VITE_ACCESS_KEY
+}`;
 
 const Gallery = () => {
-  return <div>Gallery</div>;
+  const { searchTerm } = useGlobalContext();
+  const { data, error, isPending } = useFetch(
+    `${url}&query=${searchTerm}&per_page=30`
+  );
+
+  console.log(data);
+
+  if (isPending) {
+    return (
+      <section className="image-container">
+        <h4>Loading...</h4>
+      </section>
+    );
+  }
+  if (error) {
+    return (
+      <section className="image-container">
+        <h4>{error}</h4>
+      </section>
+    );
+  }
+
+  return (
+    <section className="gallery">
+      <ul className="images">
+        {/* 이미지 반복 */}
+        {data &&
+          data.results.map((item) => {
+            const url = item?.urls?.regular;
+            return (
+              <li className="img" key={item.id}>
+                <img src={url} alt="{item.alt_description}" />
+              </li>
+            );
+          })}
+      </ul>
+    </section>
+  );
 };
 
 export default Gallery;
