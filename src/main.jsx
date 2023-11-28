74ee54306fc3fae51183bc212bf9bba61972ced6
KaengEE <yopuiqwer@naver.com>
2023-11-28 13:55:23 +0900

    context 적용

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { AppProvider } from "./context.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <AppProvider>
     <App />
-  </React.StrictMode>,
-)
+  </AppProvider>
+);
