0bdd7e8a61832aa09d6a635bb5ba1bcab4204083
KaengEE <yopuiqwer@naver.com>
2023-11-28 15:13:09 +0900

    검색어 입력

--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -1,10 +1,23 @@
 import React from "react";
+import { useGlobalContext } from "./context";
 
 const SearchForm = () => {
+  const { setSearchTerm } = useGlobalContext();
+
+  //검색이벤트
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const searchValue = e.target.elements.search.value?.trim();
+    if (searchValue !== null && searchValue.length > 1) {
+      //console.log(searchValue);
+      setSearchTerm(searchValue);
+    }
+  };
+
   return (
     <section>
       <h1 className="title">Images Gallery</h1>
-      <form className="search-form">
+      <form className="search-form" onSubmit={handleSubmit}>
         <input
           type="text"
           className="search-input"
