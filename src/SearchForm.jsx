9f2fae785e4c06d241cbcf1e64818a101aafb261
KaengEE <yopuiqwer@naver.com>
2023-11-28 14:57:25 +0900

    SearchForm css

--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const SearchForm = () => {
-  return <div>SearchForm</div>;
+  return (
+    <section>
+      <h1 className="title">Images Gallery</h1>
+      <form className="search-form">
+        <input
+          type="text"
+          className="search-input"
+          name="search"
+          placeholder="cat"
+        />
+        <button type="submit" className="btn">
+          search
+        </button>
+      </form>
+    </section>
+  );
 };
 
 export default SearchForm;
